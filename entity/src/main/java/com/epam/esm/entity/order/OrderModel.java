30dbb71ff7ceab72fff4fcc5afa562ecef381d89
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-08-04 15:31:56 +0500

    created assemblers and models

---
+++ entity/src/main/java/com/epam/esm/entity/order/OrderModel.java
@@ -0,0 +1,40 @@
+package com.epam.esm.entity.order;
+
+import com.epam.esm.entity.giftcertificate.GiftCertificate;
+import com.epam.esm.entity.giftcertificate.GiftCertificateModel;
+import com.epam.esm.entity.user.User;
+import com.epam.esm.entity.user.UserModel;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Relation(collectionRelation = "orders")
+public class OrderModel extends RepresentationModel<OrderModel> {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private int id;
+    private BigDecimal price;
+    private OrderStatus status;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime orderDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime lastUpdateDate;
+
+    private UserModel user;
+    private GiftCertificateModel giftCertificate;
+
+}
